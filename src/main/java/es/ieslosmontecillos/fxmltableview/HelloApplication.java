eac1ab936fb8a2cf0a1e35b32ad0318e962f139f
Raul Plaza Galvez <raulplaza208@gmail.com>
2023-11-15 09:20:01 +0100

    Ejercicio de tabla FXMLTableView Terminado

--- src/main/java/es/ieslosmontecillos/fxmltableview/HelloApplication.java
+++ src/main/java/es/ieslosmontecillos/fxmltableview/HelloApplication.java
@@ -1,20 +1,27 @@
 package es.ieslosmontecillos.fxmltableview;
 
 import javafx.application.Application;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+    @FXML private TableView<Person> tableView;
+    @FXML private TextField firstNameField;
+    @FXML private TextField lastNameField;
+    @FXML private TextField emailField;
     @Override
     public void start(Stage primaryStage) throws IOException {
         primaryStage.setTitle("FXML TableView Example");
-        Pane myPane = (Pane)FXMLLoader.load(getClass().getResource
-                ("fxml_tableview.fxml"));
+        Pane myPane = (Pane)FXMLLoader.load(getClass().getResource("fxml_tableview.fxml"));
         Scene myScene = new Scene(myPane);
         primaryStage.setScene(myScene);
         primaryStage.show();
@@ -22,5 +29,18 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @FXML
+    protected void addPerson(ActionEvent event) {
+        ObservableList<Person> data = tableView.getItems();
+        data.add(new Person(firstNameField.getText(),
+                lastNameField.getText(),
+                emailField.getText()
+        ));
+
+        firstNameField.setText("");
+        lastNameField.setText("");
+        emailField.setText("");
     }
 }
